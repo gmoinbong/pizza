a4c1dc135a4563cc2d8f7b4c69dbd4ef2302114a
gmo_in_bong <vladosos.scherba@gmail.com>
2022-09-20 20:25:54 +0300

    html in jsx, basic handlers and refactoring.

---
+++ src/components/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Categories() {
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex === index ? 'active' : ''}>
+            {value}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
